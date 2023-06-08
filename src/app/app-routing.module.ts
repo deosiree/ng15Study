f4180c0d951549583e8822c8899b035701754285
deosiree <1692592987@qq.com>
2023-06-08 10:44:28 +0800

    'routerUpdate'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './baserouter/home/home.component';
 
 // 01 路由配置
 const routes: Routes = [
-  { path: "", component: HomeComponent },
+  { path: "", redirectTo: 'home', pathMatch: 'full' },
   { path: "home", component: HomeComponent },
   { path: "about", component: AboutComponent }
 ];
